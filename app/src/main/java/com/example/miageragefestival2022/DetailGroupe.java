f65bbf0b3dc6a00d2d7fc3d1e0fdc473bfeab567
AntoineArthurG <antoine.g.arthur@gmail.com>
2022-10-18 18:01:44 +1100

    Création du l'activité DetailGroupe accompagné du layout

---
+++ app/src/main/java/com/example/miageragefestival2022/DetailGroupe.java
@@ -0,0 +1,14 @@
+package com.example.miageragefestival2022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailGroupe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_groupe);
+    }
+}
