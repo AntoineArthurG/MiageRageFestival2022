d0b0c3077293e2c758ea62c332e8920ea76d31e4
AntoineArthurG <antoine.g.arthur@gmail.com>
2022-10-23 19:35:30 +1100

    Evolution activité DetailGroupe.java : Ajout d'un bouton dans le groupe_card.xml implémentation pour renvoyer l'utilisateur vers la page de description du groupe choisi ref Trello : DetailGroupe

--- app/src/main/java/com/example/miageragefestival2022/DetailGroupe.java
+++ app/src/main/java/com/example/miageragefestival2022/DetailGroupe.java
@@ -2,13 +2,25 @@ package com.example.miageragefestival2022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetailGroupe extends AppCompatActivity {
+
+    private TextView titreGroupe;
+    private TextView description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_groupe);
+
+        titreGroupe = findViewById(R.id.tv_nom_groupe);
+        description = findViewById(R.id.tv_description_groupe);
+
+        Intent intent = getIntent();
+        titreGroupe.setText(intent.getStringExtra("titreGroupe"));
+
     }
 }
--- app/src/main/java/com/example/miageragefestival2022/ListeGroupeViewHolder.java
+++ app/src/main/java/com/example/miageragefestival2022/ListeGroupeViewHolder.java
@@ -1,24 +1,36 @@
 package com.example.miageragefestival2022;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 public class ListeGroupeViewHolder extends RecyclerView.ViewHolder {
 
-    private TextView nomGroupe;
+    //private TextView nomGroupe;
+    private Button nomGroupe;
 
     public ListeGroupeViewHolder(View itemView){
         super(itemView);
-        nomGroupe = (TextView) itemView.findViewById(R.id.tv_nomGroupe);
+        nomGroupe = (Button) itemView.findViewById(R.id.btn_listeGroupe);
+        nomGroupe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent detailGroupeIntent = new Intent(view.getContext() ,DetailGroupe.class);
+                detailGroupeIntent.putExtra("titreGroupe", nomGroupe.getText().toString());
+                view.getContext().startActivity(detailGroupeIntent);
+            }
+        });
     }
 
     public TextView getNomGroupe() {
         return nomGroupe;
     }
 
-    public void setNomGroupe(TextView nomGroupe) {
+    public void setNomGroupe(Button nomGroupe) {
         this.nomGroupe = nomGroupe;
     }
 }
