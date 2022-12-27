98924656c5081e230065b5094689b1bbd7fd886d
Matho <mathilde.nyikeine@outlook.fr>
2022-12-27 21:27:20 +1100

    Design Detail Groupe

--- app/src/main/java/com/example/miageragefestival2022/DetailGroupe.java
+++ app/src/main/java/com/example/miageragefestival2022/DetailGroupe.java
@@ -2,6 +2,7 @@ package com.example.miageragefestival2022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,20 +26,33 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class DetailGroupe extends AppCompatActivity {
 
     private TextView tv_titreGroupe ;
+    private TextView tv_date;
+    private TextView tv_heure;
     private TextView tv_description;
+    private TextView tv_scene;
+    private TextView tv_web;
+    private TextView tv_image;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_groupe);
 
         tv_titreGroupe = findViewById(R.id.tv_nom_groupe);
-        tv_description = findViewById(R.id.tv_description_groupe);
+        tv_date= findViewById(R.id.tv_date);
+        tv_heure = findViewById(R.id.tv_heure);
+        tv_description = findViewById(R.id.tv_description);
+        tv_scene = findViewById(R.id.tv_scene);
+        tv_web = findViewById(R.id.tv_web);
+        tv_image = findViewById(R.id.tv_image);
+
         Intent intent = getIntent();
 
         String titreGroupe = intent.getStringExtra("titreGroupe");
 
         getDetailGroupe("info/"+titreGroupe);
+        getImage("illustrations/"+titreGroupe+"/image.jpg");
     }
 
     /**
@@ -66,7 +80,40 @@ public class DetailGroupe extends AppCompatActivity {
                 );
 
                 tv_titreGroupe.setText(groupe.getData().getArtiste());
+                tv_date.setText(groupe.getData().getJour());
+                tv_heure.setText(groupe.getData().getHeure());
                 tv_description.setText(groupe.getData().getTexte());
+                tv_web.setText(groupe.getData().getWeb());
+                tv_scene.setText(groupe.getData().getScene());
+
+            }
+
+            @Override
+            public void onFailure(Call<Groupe> call, Throwable t) {
+                Log.d("TAG","Response = " + t.toString());
+            }
+        });
+    }
+
+    public void getImage(String url){
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("https://daviddurand.info/D228/festival/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        Api api = retrofit.create(Api.class);
+
+        Call<Groupe> call = api.getDetailGroupe(url);
+        call.enqueue(new Callback<Groupe>() {
+            @Override
+            public void onResponse(Call<Groupe> call, Response<Groupe> response) {
+
+                Groupe groupe = new Groupe(
+                        response.body().getCode(),
+                        response.body().getMessage(),
+                        response.body().getData()
+                );
+
+                tv_image.setText(groupe.getData().getImage());
 
             }
 
