17ea509e7c1de090fe276bd7ceda7b9a53ebe090
AntoineArthurG <antoine.g.arthur@gmail.com>
2023-01-18 00:00:24 +1100

    fix + feat : ajout de la possibilité de cliquer sur un num d'urgence pour lancer l'appel modification du .xml des numeros d'urgence afin de rendre l'ui reactif

--- app/src/main/java/com/example/miageragefestival2022/NumUrgenceActivity.java
+++ app/src/main/java/com/example/miageragefestival2022/NumUrgenceActivity.java
@@ -1,6 +1,11 @@
 package com.example.miageragefestival2022;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.miageragefestival2022.databinding.ActivityNumUrgenceBinding;
 import com.google.android.material.navigation.NavigationView;
@@ -18,5 +23,16 @@ public class NumUrgenceActivity extends DrawerBaseActivity {
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setItemIconTintList(null);
+
+        LinearLayout appelEuro = findViewById(R.id.num_appel_euro);
+        appelEuro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView tv_num = findViewById(R.id.num_euro);
+                String num = tv_num.getText().toString();
+                Intent appel = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", num, null));
+                startActivity(appel);
+            }
+        });
     }
 }
